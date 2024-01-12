c0ae81430c524ec104d96e75fa02f0d6969a3134
chaeichi <chaeichi427@gmail.com>
2024-01-13 04:15:05 +0900

    예제 3-6 /ch3/OperatorEx6.java

---
+++ ch03/src/OperatorEx6.java
@@ -0,0 +1,10 @@
+class OperatorEx6 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        // byte c = a + b;
+        // 컴파일 에러 발생
+
+        // System.out.println(c);
+    }
+}
