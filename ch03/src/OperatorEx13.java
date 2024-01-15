24a30fe053338361a5110da9fe03dff9561d2671
chaeichi <chaeichi427@gmail.com>
2024-01-15 13:44:03 +0900

    예제 3-13 /ch3/OperatorEx13.java 오타 수정

--- ch03/src/OperatorEx13.java
+++ ch03/src/OperatorEx13.java
@@ -1,7 +1,7 @@
 class OperatorEx13 {
     public static void main(String[] args) {
         char c1 = 'a';
-        // char c2 = c + 1; // 컴파일 에러 발생
+        // char c2 = c1 + 1; // 컴파일 에러 발생
         char c2 = 'a' + 1; // 컴파일 에러 없음
 
         System.out.println(c2);
