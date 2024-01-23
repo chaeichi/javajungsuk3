6fc9dbc0db1ca726d1847e2ef4cd7d8ffc57d652
chaeichi <chaeichi427@gmail.com>
2024-01-24 02:03:53 +0900

    연습문제 3-9 /ch3/Exercise3_9.java

---
+++ ch03/src/Exercise3_9.java
@@ -0,0 +1,8 @@
+class Exercise3_9 {
+    public static void main(String[] args) {
+        char ch = 'z';
+        boolean b = (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9');
+
+        System.out.println(b);
+    }
+}
