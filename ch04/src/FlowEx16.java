f7ab42a7004fcd8d8988c15c4977cc608f8cc09f
chaeichi <chaeichi427@gmail.com>
2024-01-29 18:58:55 +0900

    예제 4-16 /ch4/FlowEx16.java

--- ch04/src/FlowEx16.java
+++ ch04/src/FlowEx16.java
@@ -2,7 +2,7 @@ class FlowEx16 {
     public static void main(String[] args) {
         for(int i=1; i<=5; i++) {
             for(int j=1; j<=10; j++) {
-                System.out.printf("*");
+                System.out.print("*");
             }
             System.out.println();
         }
