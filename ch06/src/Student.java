3a38b98a59b152e10f9a1ca67ae537f83f5d1eb4
chaeichi <chaeichi427@gmail.com>
2024-08-02 15:56:12 +0900

    연습문제 6-5 /ch6/Exercise6_5.java

--- ch06/src/Student.java
+++ ch06/src/Student.java
@@ -6,6 +6,28 @@ class Student {
     int eng; // 영어점수
     int math; // 수학점수
 
+    Student() { }
+
+    Student(String name, int ban, int no, int kor, int eng, int math) {
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+
+    String info() {
+        return name
+                + ", " + ban
+                + ", " + no
+                + ", " + kor
+                + ", " + eng
+                + ", " + math
+                + ", " + getTotal()
+                + ", " + getAverage();
+    }
+
     int getTotal() {
         return kor + eng + math;
     }
