b72678a2c24f2a25c5e6cc84bcef1cddf1b6488a
chaeichi <chaeichi427@gmail.com>
2024-08-29 17:33:59 +0900

    예제 7-38 /ch7/InnerEx8.java

---
+++ ch07/src/InnerEx8.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class InnerEx8 {
+    public static void main(String[] args) {
+        Button b = new Button("start");
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("ActionEvent occurred!!!");
+            }
+        });
+    }
+}
